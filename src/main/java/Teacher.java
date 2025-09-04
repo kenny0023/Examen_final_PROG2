778541b1171b43ec54322e995cc4f90e6fed1885
kenny0023 <hei.bryan.2@gmail.com>
2025-09-04 09:56:14 +0200

    ajout de Teacher

---
+++ src/main/java/Teacher.java
@@ -0,0 +1,15 @@
+public class Teacher {
+    private int id;
+    private String lastName;
+    private String firstName;
+
+    public Teacher(int id, String lastName, String firstName) {
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
